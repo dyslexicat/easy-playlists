bd29cf05868bba073cd1e21e9d2037d0ba409bbc
dyslexicat <dyslexicat@gmail.com>
2018-02-10 18:41:13 +0300

    Changed a line that pointed to the tutorial app instead of mine

--- src/App.js
+++ src/App.js
@@ -124,7 +124,7 @@ class App extends Component {
            </div> : <button onClick={() => {
                window.location = window.location.href.includes('localhost')
                  ? 'http://localhost:8888/login'
-                 : 'https://better-playlists-backend.herokuapp.com/login' }
+                 : 'https://easy-playlists-backend.herokuapp.com/login' }
              }
              style={{padding: '20px', 'font-size': '50px', 'margin-top': '20px'}}>Sign in with Spotify</button>
            }
